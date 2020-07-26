bec9bd2fab2912e75585f3b2223ed81efcbf1c21
Daniel Figucio <745341+affrae@users.noreply.github.com>
2020-07-26 16:28:03 +1000

    v1 release

--- index.js
+++ index.js
@@ -10,25 +10,18 @@ async function run() {
     const key = core.getInput('key');
     const myToken = core.getInput('myToken');
     const octokit = github.getOctokit(myToken);
-    console.log(issue['owner']);
-    console.log(issue['repo']);
-    console.log(issue['number']);
 
     const body = (await octokit.issues.get({owner:issue['owner'],repo:issue['repo'],issue_number:issue['number']})).data.body;
 
-    console.log(body);
 
     const match = body.match(regex)
 
     if (match) {
       const data = JSON.parse(match[1])
-      console.log("Found custom metadata")
-      console.log("Returning: " + (key ? data && data[key] : data))
 
       core.setOutput('result', key ? data && data[key] : data)
 
     }
-    console.log("Did not find custom metadata")
 
   } catch (error) {
     core.setFailed(error.message);
